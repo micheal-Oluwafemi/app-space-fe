c5e4ae29c665703740802ac385e486bf7da64b91
micheal Oluwafemi <raytech401@gmail.com>
2025-05-06 16:46:55 +0100

    Fixed deployment bug in Congratulations component

--- src/app/congratulations/page.tsx
+++ src/app/congratulations/page.tsx
@@ -18,53 +18,55 @@ const Page = () => {
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <AutoConfetti />
       <Suspense fallback={<div className="loader"></div>}>
-        {(() => {
-          const type = SearchParamsWrapper();
-          return type === "welcome" ? (
-            <div className="flex flex-col items-center gap-4">
-              <div className="flex flex-col items-center justify-center gap-3">
-                <h1 className="text-5xl">🎉</h1>
-              </div>
-
-              <div className="mt-3 w-full space-y-2">
-                <h2 className="text-center text-2xl font-semibold text-black/80 lg:text-3xl">
-                  Congratulations
-                </h2>
-                <p className="text-center">
-                  You've successfully created your AppSpace Account{" "}
-                </p>
-              </div>
-
-              <Link
-                href="/dashboard"
-                className="bg-baseColor hover:bg-baseColor-light w-fit rounded-md px-4 py-2 text-center font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
-              >
-                Continue
-              </Link>
-            </div>
-          ) : (
-            <div className="flex flex-col items-center gap-4">
-              <div className="flex flex-col items-center justify-center gap-3">
-                <h1 className="text-5xl">🎉</h1>
-              </div>
-
-              <div className="mt-3 w-full space-y-2">
-                <h2 className="text-center text-3xl font-semibold text-black/80">
-                  All done
-                </h2>
-                <p className="text-center">Your password has been reset.</p>
-              </div>
-
-              <Link
-                href="/auth/login"
-                className="bg-baseColor hover:bg-baseColor-light w-full rounded-md px-4 py-3 text-center font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
-              >
-                Continue to Login
-              </Link>
-            </div>
-          );
-        })()}
+        <SearchParamsWrapperContent />
       </Suspense>
+    </div>
+  );
+};
+
+const SearchParamsWrapperContent = () => {
+  const type = SearchParamsWrapper();
+  return type === "welcome" ? (
+    <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <h1 className="text-5xl">🎉</h1>
+      </div>
+
+      <div className="mt-3 w-full space-y-2">
+        <h2 className="text-center text-2xl font-semibold text-black/80 lg:text-3xl">
+          Congratulations
+        </h2>
+        <p className="text-center">
+          You've successfully created your AppSpace Account{" "}
+        </p>
+      </div>
+
+      <Link
+        href="/dashboard"
+        className="bg-baseColor hover:bg-baseColor-light w-fit rounded-md px-4 py-2 text-center font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
+      >
+        Continue
+      </Link>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <h1 className="text-5xl">🎉</h1>
+      </div>
+
+      <div className="mt-3 w-full space-y-2">
+        <h2 className="text-center text-3xl font-semibold text-black/80">
+          All done
+        </h2>
+        <p className="text-center">Your password has been reset.</p>
+      </div>
+
+      <Link
+        href="/auth/login"
+        className="bg-baseColor hover:bg-baseColor-light w-full rounded-md px-4 py-3 text-center font-medium text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500"
+      >
+        Continue to Login
+      </Link>
     </div>
   );
 };
